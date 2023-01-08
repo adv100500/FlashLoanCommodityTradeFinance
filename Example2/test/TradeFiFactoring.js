c0f8d21a6ccec3e88b7710150f84613c868fc36b
adv100500 <121932525+adv100500@users.noreply.github.com>
2023-01-08 23:10:44 +0100

    Update TradeFiFactoring.js

--- Example2/test/TradeFiFactoring.js
+++ Example2/test/TradeFiFactoring.js
@@ -1,14 +1,5 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
-/*
-A new cool lending pool has launched! It's now offering flash loans of DVT tokens.
-
-Wow, and it even includes a really fancy governance mechanism to control it.
-
-What could go wrong, right ?
-
-You start with no DVT tokens in balance, and the pool has 1.5 million. Your objective: take them all.
-*/
 
 describe('[Trade Finance Flash loan]', function () {
     let buyer, bank, captain, borrower, offtaker,traderInitialBalance,supplierInitialBalance,offtakerInitialBalance, factoringInitialBalance;
