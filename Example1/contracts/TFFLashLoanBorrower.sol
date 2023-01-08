805e1f0b9ea2e1074a74270bafdf983a20710a74
adv100500 <121932525+adv100500@users.noreply.github.com>
2023-01-08 23:47:20 +0100

    Update TFFLashLoanBorrower.sol

--- Example1/contracts/TFFLashLoanBorrower.sol
+++ Example1/contracts/TFFLashLoanBorrower.sol
@@ -40,7 +40,7 @@ contract FlashLoanBorrowerTrader {
         
         _executeActionDuringFlashLoan(msg.value);
 
-        require(address(this).balance >= amountToBeRepaid, "Cannot borrow that much");
+        require(address(this).balance >= amountToBeRepaid, "Not enough funds to repay the flash loan");
         
         // Return funds to FlashLoanBank
         FlashLoanBank.sendValue(amountToBeRepaid);
